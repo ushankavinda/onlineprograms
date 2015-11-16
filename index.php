e3ed2b677adfcd5df6450b9299a4649855f4b640
ushankavinda <ushanrocky@gmail.com>
2015-11-16 14:12:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 ﻿<!DOCTYPE html>
 <html>
     <head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Online Program</title>
         
         <link rel="stylesheet" href="_/styles/bootstrap.min.css" />
